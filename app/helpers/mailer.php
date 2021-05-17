1e2729729f3be1b0c4557770e48ca74f6a23f5ba
Mark1267 <markwillams1267@gmail.com>
2021-05-17 08:35:20 +0100

    u17052021

--- app/helpers/mailer.php
+++ app/helpers/mailer.php
@@ -49,8 +49,8 @@ try{
         $mail->isSMTP();                                            // Send using SMTP
         $mail->Host       = 'smtp.hostinger.com';                    // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'support@rocktera-assets.com';                     // SMTP username
-        $mail->Password   = 'Stafen12345';                               // SMTP password
+        $mail->Username   = MUSER;                    // SMTP username
+        $mail->Password   = MPASS;                               // SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption PHPMailer::ENCRYPTION_SMTPS; `` encouraged 
         $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_STARTTLS` above 587
     
