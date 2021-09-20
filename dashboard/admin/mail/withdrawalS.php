0f3e03608b3eb5b50bdb3fa51b4fe48069c00554
Mark Willams <81035184+Mark1267@users.noreply.github.com>
2021-09-20 02:30:11 +0100

    Update withdrawalS.php

--- dashboard/admin/mail/withdrawalS.php
+++ dashboard/admin/mail/withdrawalS.php
@@ -10,21 +10,21 @@
 
 <body style="padding: 0; margin: 0; background: rgb(248, 248, 248); font-family: Arial; font-size: 15px;">
     <div class="container" style="background-color:white; max-width: 1000px; margin: 0px auto; position: relative;">
-        <div class="row" style=" padding: 10px; background: #bde884;">
-            <div class="col-4 mx-auto" style="margin: 0px auto; width: 66.66%;">
+        <div style=" padding: 10px; background: #bde884;">
+            <div style="margin: 0px auto; width: 66.66%;">
                 <img src="#logo#" alt="rockteraassets_logo" class="img-fluid" style="width: 100%; vertical-align: middle;">
             </div>
         </div>
-        <div class="row" style="position: relative; padding: 25px;">
-            <div class="col-12" style="width: 100% !important; font-size: 30px;">
+        <div style="position: relative; padding: 25px;">
+            <div style="width: 100% !important; font-size: 30px;">
                 <h4 style="text-align: center; text-transform: capitalize; margin: 0;">Withdrawal Successful.</h4>
             </div>
-            <div class="col-10 mx-auto " style="width: 93.33%; display: block; margin: 0px auto;">
+            <div style="width: 93.33%; display: block; margin: 0px auto;">
                 <h5>Hi #name#, </h5>
             </div>
-            <div class="col-12" style="padding: 10px; word-wrap: break-word;">
-                <span style="line-height: 20px;">Thank you for your withdrawal!. We've received your withdrawal request Transaction ID: <span style="font-family: monospace;">#trans_id#</span>. And it has been proccesed successfully.</span>
-                <a href="https://rocktera-assets/signin" class="btn" style="margin: 15px auto; display: block; text-align: center; padding: 16px 30px; border-radius: 4px; color: white; background-color: #73ae20;; text-decoration: none;">Dashboard</a>
+            <div style="padding: 10px; word-wrap: break-word;">
+                <span style="line-height: 20px;">Thank you for your withdrawal!. We've accepted your withdrawal request Transaction ID: <span style="font-family: monospace;">#trans_id#</span>. And it has been proccesed successfully.</span>
+                <a href="https://rocktera-assets/signin" class="btn" style="margin: 15px auto; display: block; text-align: center; padding: 16px 30px; border-radius: 4px; color: white; background-color: #73ae20; text-decoration: none;">Dashboard</a>
                 <center style="margin-top: 40px;"><span style="margin-top: 30px;">Invoice Details</span></center>
                 <table cellspacing="3" style="width: 100%; text-align: left; border: 2px dashed #bde884;">
                 <tr style="padding: 10px; background-color:  #d6f0b3;">
@@ -33,7 +33,7 @@
                 </tr>
                 <tr style="padding: 10px; background-color:  #d6f0b3;">
                     <th style="padding: 10px; background-color: #cbed9c; width: 40%;">Transaction Type</th>
-                    <td style="padding: 10px;">Deposit Successful</td>
+                    <td style="padding: 10px;">Withdrawal Successful</td>
                 </tr>
                 <tr style="padding: 10px; background-color:  #d6f0b3;">
                     <th style="padding: 10px; background-color: #bde884; width: 40%;">Transaction Amount</th>
