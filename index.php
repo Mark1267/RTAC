6f8a762179146978f515323dd98ee715992cee24
Ekwueme Ugochukwu <57512572+Ugochukwu07@users.noreply.github.com>
2024-11-24 07:57:24 -0500

    nargin

--- index.php
+++ index.php
@@ -342,11 +342,11 @@ $title = 'Home';
       <div class="col-12 col-md-6 text-center mx-auto" style="padding: 20px;">
         <h2 class="mb-4">Get The Highlight of ROCKTERA ASSETS</h2><br>
         <!-- <h5 class=" border_radius" style="border: 2px solid white; padding: 10px; display: inline-block;"><i class="fa fa-download" aria-hidden="true"></i> Download Below</h5><br><br> -->
-      <video width="100%" height="240" controls loop>
+      <video width="auto" height="240" controls loop>
         <source src="https://rocktera-assets.net/assets/open/video/IMG_5275.MP4" type="video/mp4">
         Your browser does not support the video tag.
-      </video>
-        <a  class="btn-white text-uppercase border_radius" href="https://rocktera-assets.net/assets/open/pdf/"> <i class="fa fa-download" aria-hidden="true"></i> Get To Know Us More</a>
+      </video> <br>
+        <!-- <a  class="btn-white text-uppercase border_radius" href="https://rocktera-assets.net/assets/open/pdf/"> <i class="fa fa-download" aria-hidden="true"></i> Get To Know Us More</a> -->
       </div>
     </div>
   </section>
