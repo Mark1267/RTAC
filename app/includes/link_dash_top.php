4524bf0878163c77402d077ac04b7c63edc6ff45
Ekwueme Ugochukwu <57512572+Ugochukwu07@users.noreply.github.com>
2024-11-24 19:54:31 -0500

    Fixed Alginment V

--- app/includes/link_dash_top.php
+++ app/includes/link_dash_top.php
@@ -43,7 +43,7 @@
         var _smartsupp = _smartsupp || {};
         _smartsupp.key = '06ceb9e3842e62c48a08dd89b870e46d78c18d4a';
         window.smartsupp||(function(d) {
-            var s,c,o=smartsupp=function(){ o..push(arguments)};o.=[];
+            var s,c,o=smartsupp=function(){ o.push(arguments)};o.=[];
             s=d.getElementsByTagName('script')[0];c=d.createElement('script');
             c.type='text/javascript';c.charset='utf-8';c.async=true;
             c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
